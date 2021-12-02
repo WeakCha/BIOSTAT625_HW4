d9cc423f51cec1b7c49ab99c161aacf2dc36f5b0
Li Liu <weakcha@umich.edu>
2021-12-01 19:17:19 -0500

    update 12.1

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,18 @@
 library(testthat)
 library(BIOSTAT625)
 
+#data <- read.csv("data/heart.csv")
+
+install.packages("dplyr", repos = "http://cran.us.r-project.org")
+install.packages("lattice", repos = "http://cran.us.r-project.org")
+install.packages("Matrix", repos = "http://cran.us.r-project.org")
+install.packages("survival", repos = "http://cran.us.r-project.org")
+install.packages("glmnet", repos = "http://cran.us.r-project.org")
+
+library(dplyr)
+library(lattice)
+library(Matrix)
+library(glmnet)
+library(survival)
+
 test_check("BIOSTAT625")
